25125c925a8e6e603ead78982d1ae5ea39a6e11e
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-09-14 12:35:39 -0400

    completed the characterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Card, Icon, Image } from 'semantic-ui-react';
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  
+  const {name, status, species, origin, loction, characterImg} = props;
+
+  return (
+    <Card>
+      <Image src={characterImg} wrapped ui={false}/>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>{species} {status}</Card.Meta>
+        <Card.Description>Location: {location}</Card.Description>
+        <Card.Description> Origin: {origin}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon Name='tv'/> Episodes
+      </Card.Content>
+    </Card>
+  )
 }
