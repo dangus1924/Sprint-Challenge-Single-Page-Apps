ebeaed659ef542874553e2c4ab87e0d533a0a442
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-09-14 14:08:05 -0400

    mvp completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import MainRoute from './components/MainRoute.js';
+import SearchForm from './components/SearchForm.js'
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      <Header> 
+        <searchForm/> 
+      </Header>
+
       <TabNav />
       <MainRoute />
     </main>
